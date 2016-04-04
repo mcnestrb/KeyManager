9f7561803c06c5ca8efc48a35b1a729042dc9fbf
Brian McNestry <brianmcnestry@gmail.com>
2016-04-04 22:45:48 +0100

    add group mem relationships

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ActiveRecord::Base
+    has_many :users, through: :group_mems
+    has_many :group_mems
 end
--- app/models/group_mem.rb
+++ app/models/group_mem.rb
@@ -1,2 +1,4 @@
 class GroupMem < ActiveRecord::Base
+    belongs_to :group
+    belongs_to :user
 end
